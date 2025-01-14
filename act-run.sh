1d7448d60cc3af77f7f29bdeade99be1f47c84d3
DHL-Sheffield <dhlee1@sheffield.ac.uk>
2025-01-14 15:26:06 +0000

    restarting act run with high l planck data included

--- act-run.sh
+++ act-run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
-#SBATCH --cpus-per-task=8
-#SBATCH --time=08:00:00
-#SBATCH --mem=16G
+#SBATCH --time=96:00:00
+#SBATCH --mem=32G
 #SBATCH --mail-type=fail,end
 #SBATCH --mail-user=dhlee1@sheffield.ac.uk
 
