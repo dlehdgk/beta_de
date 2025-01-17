71c6a58073bb794e2586130f517787e489598c68
DHL-Sheffield <dhlee1@sheffield.ac.uk>
2025-01-17 10:47:06 +0000

    R-1=0.2261

--- act-run.sh
+++ act-run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
-#SBATCH --time=96:00:00
-#SBATCH --mem=32G
+#SBATCH --time=12:00:00
+#SBATCH --mem=16G
 #SBATCH --mail-type=fail,end
 #SBATCH --mail-user=dhlee1@sheffield.ac.uk
 
